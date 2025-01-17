5e962858c01f5bfd2c95577df80572832c6d1e75
matty-04 <129518838+matty-04@users.noreply.github.com>
2025-01-17 20:35:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+alert("Happy Monthsary Baby ko! 😘💞")
+
+function surprise(){
+  alert("Happy Monthsary Aaliyah ko! sorry eto lang kinayanan kasi wala me money😭 I love you always my one and only 😘🫵💞")
+}
